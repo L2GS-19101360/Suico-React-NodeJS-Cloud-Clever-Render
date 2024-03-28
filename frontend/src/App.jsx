fe87a2e80a02ae58bf4a1935afc79db6a5f4dcbf
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-03-28 14:48:22 +0800

    Testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
-import { Component, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Component, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { Navbar, Container } from 'react-bootstrap';
 
 class App extends Component {
   constructor(){
@@ -10,11 +11,11 @@ class App extends Component {
 
   render(){
     return(
-      <div>
-        Hello World!
+      <div style={{ backgroundColor: 'lightblue' }}>
+        {/* Your page content goes here */}
       </div>
     );
   }
 }
 
-export default App
+export default App;
