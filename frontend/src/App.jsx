26b198d5276d28ec762ed1d18f2adec92ac9cb9f
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-03-28 15:47:06 +0800

    Fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { Component, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Navbar, Container } from 'react-bootstrap'
 import CreatePersonModal from './components/CreatePersonModal'
