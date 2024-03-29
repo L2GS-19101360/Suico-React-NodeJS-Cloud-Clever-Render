b2f695e45f0acfb91c2d7746747a699606afcfa9
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-03-29 15:07:01 +0800

    Updated

--- frontend/src/components/PersonsTable.jsx
+++ frontend/src/components/PersonsTable.jsx
@@ -1,5 +1,5 @@
 import { Component, useState } from 'react'
-import { Table, Button } from 'react-bootstrap'
+import { Table, Button, Modal, Form } from 'react-bootstrap'
 import axios from 'axios';
 
 class PersonsTable extends Component {
@@ -7,7 +7,13 @@ class PersonsTable extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            persons: []
+            persons: [],
+
+            selectedID: "",
+            selectedFirstName: "",
+            selectedLastName: "",
+
+            showModal: false
         }
     }
 
@@ -27,8 +33,42 @@ class PersonsTable extends Component {
 
     }
 
+    handleModalOpen = (id, firstname, lastname) => {
+        console.log(id + firstname + lastname);
+
+        this.setState({
+            showModal: true,
+            selectedID: id,
+            selectedFirstName: firstname,
+            selectedLastName: lastname
+        });
+    }
+
+    handleModalClose = () => {
+        this.setState({ showModal: false, selectedID: null, selectedFirstName: "", selectedLastName: "" });
+    }
+
     toDeletePerson = async (id) => {
         console.log(id);
+
+        axios.delete(
+            `https://react-node-mysql-api.onrender.com/api/v1/persons/${id}`
+        ).then(
+            (response) => {
+                console.log("Server Response", response.data);
+                window.location.reload();
+            }
+        ).catch(
+            (error) => {
+                console.log(error);
+            }
+        );
+    }
+
+    toUpdatePerson = async (event) => {
+        event.preventDefault();
+
+        console.log(this.state.selectedID + this.state.selectedFirstName + this.state.selectedLastName);
     }
 
     render() {
@@ -50,12 +90,40 @@ class PersonsTable extends Component {
                                 <td>{person.id}</td>
                                 <td>{person.firstname}</td>
                                 <td>{person.lastname}</td>
-                                <td><Button variant="warning">Update</Button></td>
+                                <td><Button variant="warning" onClick={(event) => this.handleModalOpen(person.id, person.firstname, person.lastname)}>Update</Button></td>
                                 <td><Button variant="danger" onClick={() => this.toDeletePerson(person.id)}>Delete</Button></td>
                             </tr>
                         ))}
                     </tbody>
                 </Table>
+
+                <Modal show={this.state.showModal} onHide={this.handleModalClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Update Contact {this.state.selectedID}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form>
+                            <Form.Label>First Name</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name='newFirstName'
+                                value={this.state.selectedFirstName}
+                                onChange={(e) => this.setState({ selectedFirstName: e.target.value })} />
+                            <Form.Label>Last Name</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name='newFirstName'
+                                value={this.state.selectedLastName}
+                                onChange={(e) => this.setState({ selectedLastName: e.target.value })} /><br/>
+                            <Button variant="warning" type='submit' onClick={this.toUpdatePerson}>Create Person</Button>
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleModalClose}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
         );
     }
